55182d1db5e7be1bb149903ed2ccdd398d0b5723
Rene van der Ark <r.van.der.ark@gmail.com>
2016-09-26 09:47:16 +0200

    initial edit gui component.

---
+++ components/edit-gui/edit-gui.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Page from "../page.jsx";
+
+class EditGui extends React.Component {
+
+	render() {
+
+		return (
+			<Page>
+				
+				<div type="footer-body">
+				</div>
+			</Page>
+		)
+	}
+}
+
+export default EditGui;
